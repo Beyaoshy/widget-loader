d12e9be91c230b49e49c22bdeb835468b9f2206e
Beyaoshy <142740640+Beyaoshy@users.noreply.github.com>
2025-07-31 00:17:41 +0500

    Add files via upload

---
+++ stealth.js
@@ -0,0 +1,10 @@
+<script>
+ window.LOCALO_FREE_TOOL = {
+   container: "#free-tool",
+   hidePoweredBy: true,
+   token: "FrBHJZclLSY-e6a5cGmha1o4qKRXse5Iuab81WYq3pE"
+ };
+</script>
+<script src="https://jstools.localo.app/scripts/freetool.js" async defer></script>
+
+<div id="free-tool">Loading...</div>
